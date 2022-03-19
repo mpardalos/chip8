98f8d0027648627cb609bc25d8b4f3fcbcde1d45
Michalis Pardalos <m.pardalos@gmail.com>
2022-03-19 19:44:37 +0000

    Processor state, loading initial RAM, stepping (without executing)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,49 @@
 mod instruction;
 
-use std::{env::args, fs};
+use std::{env::args, fs, process::exit};
 
 use crate::instruction::Instruction;
+
+#[derive(Debug)]
+struct CHIP8 {
+    reg: [u8; 16],
+    idx: u16,
+    pc: u16,
+
+    mem: Box<[u8; 4096]>,
+}
+
+impl CHIP8 {
+    fn new(instruction_section: &[u8]) -> CHIP8 {
+        let mut mem = Box::new([0; 4096]);
+        mem[0x200..0x200 + instruction_section.len()].copy_from_slice(instruction_section);
+
+        CHIP8 {
+            reg: [0; 16],
+            idx: 0,
+            pc: 0x200,
+            mem,
+        }
+    }
+
+    fn step(&mut self) {
+        let instr_bits =
+            u16::from_be_bytes([self.mem[self.pc as usize], self.mem[self.pc as usize + 1]]);
+
+        let instr_decode = Instruction::from_bits(instr_bits);
+
+        match instr_decode {
+            Some(Instruction::SYS(0)) => {
+                println!("Null, exiting");
+                exit(0);
+            }
+            Some(instr) => println!("Executing {:?}", instr),
+            None => println!("Unknown instruction {:#x}", instr_bits),
+        }
+
+        self.pc += 2;
+    }
+}
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -11,18 +52,32 @@ fn main() {
 
     println!("Reading file {}", filename);
 
-    let start_mem: Vec<u8> = fs::read(filename).expect("open input file");
+    let instruction_mem: Vec<u8> = fs::read(filename).expect("open input file");
 
-    let instructions: Vec<Instruction> = start_mem
-        .chunks_exact(2)
-        .into_iter()
-        .map(|a| u16::from_be_bytes([a[0], a[1]]))
-        .flat_map(Instruction::from_bits)
-        .collect();
+    {
+        let instructions: Vec<Option<Instruction>> = instruction_mem
+            .chunks_exact(2)
+            .into_iter()
+            .map(|a| u16::from_be_bytes([a[0], a[1]]))
+            .map(Instruction::from_bits)
+            .collect();
 
-    let mut addr = 0x200;
-    for instruction in instructions {
-        println!("{:#x}: {:?}", addr, instruction);
-        addr += 2;
+        println!("---");
+        println!("Instructions: ");
+        let mut addr = 0x200;
+        for instruction in instructions {
+            println!("{:#x}: {:?}", addr, instruction);
+            addr += 2;
+        }
+    }
+
+    {
+        println!("---");
+        println!("CPU");
+
+        let mut cpu = CHIP8::new(&instruction_mem);
+        loop {
+            cpu.step();
+        }
     }
 }
