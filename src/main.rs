bd5f34fd84fc3fcc4a4efd3f9ce0d396036e91dd
Michalis Pardalos <m.pardalos@gmail.com>
2022-03-19 23:36:40 +0000

    Implement most basic instructions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 mod instruction;
 
-use std::{env::args, fs, process::exit};
+use std::{
+    env::args,
+    fmt::{self, Display},
+    fs,
+};
+
+use rand::prelude::*;
 
 use crate::instruction::Instruction;
 
@@ -27,6 +33,11 @@ impl Display for CHIP8 {
     }
 }
 
+enum Continue {
+    Continue,
+    Stop,
+}
+
 impl CHIP8 {
     fn new(instruction_section: &[u8]) -> CHIP8 {
         let mut mem = Box::new([0; 4096]);
@@ -40,22 +51,150 @@ impl CHIP8 {
         }
     }
 
-    fn step(&mut self) {
-        let instr_bits =
-            u16::from_be_bytes([self.mem[self.pc as usize], self.mem[self.pc as usize + 1]]);
+    fn advance(&mut self, amount: u16) -> Result<Continue, String> {
+        self.pc += amount;
+        Ok(Continue::Continue)
+    }
 
-        let instr_decode = Instruction::from_bits(instr_bits);
+    fn instruction_word_at(&self, addr: u16) -> u16 {
+        u16::from_be_bytes([self.mem[addr as usize], self.mem[addr as usize + 1]])
+    }
 
-        match instr_decode {
-            Some(Instruction::SYS(0)) => {
-                println!("Null, exiting");
-                exit(0);
+    fn step(&mut self) -> Result<Continue, String> {
+        use crate::Continue::*;
+        use Instruction::*;
+
+        let instr_bits = self.instruction_word_at(self.pc);
+        let instr = Instruction::from_bits(instr_bits)
+            .ok_or(format!("Unknown instruction {:#x}", instr_bits))?;
+
+        match instr {
+            MOVE(x, y) => {
+                self.reg[x as usize] = self.reg[y as usize];
+                self.advance(2)
             }
-            Some(instr) => println!("Executing {:?}", instr),
-            None => println!("Unknown instruction {:#x}", instr_bits),
+            OR(x, y) => {
+                self.reg[x as usize] |= self.reg[y as usize];
+                self.advance(2)
+            }
+            AND(x, y) => {
+                self.reg[x as usize] &= self.reg[y as usize];
+                self.advance(2)
+            }
+            XOR(x, y) => {
+                self.reg[x as usize] ^= self.reg[y as usize];
+                self.advance(2)
+            }
+            ADDR(x, y) => {
+                match self.reg[x as usize].checked_add(self.reg[y as usize]) {
+                    Some(val) => {
+                        self.reg[x as usize] = val;
+                        self.reg[0xf] = 0;
+                    }
+                    None => {
+                        self.reg[x as usize] = 0;
+                        self.reg[0xf] = 1;
+                    }
+                }
+                self.advance(2)
+            }
+            SUB(x, y) => {
+                self.reg[x as usize] -= self.reg[y as usize];
+                self.advance(2)
+            }
+            SHR(x, y) => {
+                self.reg[0x0F] = self.reg[y as usize] & 1;
+                self.reg[y as usize] = self.reg[x as usize] >> 1;
+                self.advance(2)
+            }
+            SHL(x, y) => {
+                self.reg[0x0F] = self.reg[y as usize] & 0xE0;
+                self.reg[y as usize] = self.reg[x as usize] << 1;
+                self.advance(2)
+            }
+            LOAD(x, n) => {
+                self.reg[x as usize] = n;
+                self.advance(2)
+            }
+            ADD(x, n) => {
+                self.reg[x as usize] += n;
+                self.advance(2)
+            }
+            // Subroutines
+            CALL(_) => Err("Subroutines".to_string()),
+            RTS => Err("Subroutines".to_string()),
+            // Jumps
+            JUMP(ofs) => {
+                self.pc = (self.pc & 0xF000) | (ofs & 0x0FFF);
+                Ok(Continue)
+            }
+            JUMPI(addr) => {
+                self.pc = addr + self.reg[0] as u16;
+                Ok(Continue)
+            }
+            // Skip
+            SKE(x, n) => {
+                if self.reg[x as usize] == n {
+                    self.advance(4)
+                } else {
+                    self.advance(2)
+                }
+            }
+            SKNE(x, n) => {
+                if self.reg[x as usize] != n {
+                    self.advance(4)
+                } else {
+                    self.advance(2)
+                }
+            }
+            SKRE(x, y) => {
+                if self.reg[x as usize] != self.reg[y as usize] {
+                    self.advance(4)
+                } else {
+                    self.advance(2)
+                }
+            }
+            SKRNE(x, y) => {
+                if self.reg[x as usize] != self.reg[y as usize] {
+                    self.advance(4)
+                } else {
+                    self.advance(2)
+                }
+            }
+            // Memory
+            STOR(_) => Err("Memory".to_string()),
+            READ(_) => Err("Memory".to_string()),
+            // Input
+            SKPR(_) => Err("Input".to_string()),
+            SKUP(_) => Err("Input".to_string()),
+            // Delays
+            MOVED(_) => Err("Delays".to_string()),
+            KEYD(_) => Err("Delays".to_string()),
+            LOADD(_) => Err("Delays".to_string()),
+            LOADS(_) => Err("Delays".to_string()),
+            // Index register
+            ADDI(x) => {
+                self.idx += self.reg[x as usize] as u16;
+                self.advance(2)
+            }
+            LOADI(addr) => {
+                self.idx = addr;
+                self.advance(2)
+            }
+            // Screen
+            DRAW(_, _, _) => Err("Screen".to_string()),
+            CLR => Err("Screen".to_string()),
+            // Other
+            LDSPR(_) => Err("LDSPR".to_string()),
+            BCD(_) => Err("BCD".to_string()),
+            RAND(x, n) => {
+                let mut rng = rand::thread_rng();
+                self.reg[x as usize] = rng.gen_range(0..n);
+                self.advance(2)
+            }
+            SYS(0) => Ok(Stop),
+            SYS(_) => Err("SYS".to_string()),
         }
-
-        self.pc += 2;
     }
 }
 
@@ -68,7 +207,7 @@ fn main() {
 
     let instruction_mem: Vec<u8> = fs::read(filename).expect("open input file");
 
-    {
+    if false {
         let instructions = instruction_mem
             .chunks_exact(2)
             .into_iter()
@@ -96,7 +235,18 @@ fn main() {
 
         let mut cpu = CHIP8::new(&instruction_mem);
         loop {
-            cpu.step();
+            println!("{}", cpu);
+            match cpu.step() {
+                Ok(Continue::Stop) => {
+                    println!("Done!");
+                    break;
+                }
+                Ok(Continue::Continue) => {}
+                Err(err) => {
+                    println!("Error: {}", err);
+                    break;
+                }
+            }
         }
     }
 }
